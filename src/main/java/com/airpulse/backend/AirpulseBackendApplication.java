84a1f4c2f359f8bbf78d4d27d4959818a5e69a6e
Pelin <kaynarcapelin@hotmail.com>
2025-04-06 14:29:18 +0300

    init: project setup

---
+++ src/main/java/com/airpulse/backend/AirpulseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.airpulse.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirpulseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirpulseBackendApplication.class, args);
+	}
+
+}
