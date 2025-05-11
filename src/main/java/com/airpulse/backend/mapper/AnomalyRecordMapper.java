9d60b52adf58219aa2d7e827c4454e04b725451f
Pelin <kaynarcapelin@hotmail.com>
2025-05-11 15:09:13 +0300

    refactor(mapper): remove some mappings

--- src/main/java/com/airpulse/backend/mapper/AnomalyRecordMapper.java
+++ src/main/java/com/airpulse/backend/mapper/AnomalyRecordMapper.java
@@ -13,16 +13,14 @@ public interface AnomalyRecordMapper {
     AnomalyRecordMapper INSTANCE = Mappers.getMapper(AnomalyRecordMapper.class);
 
     @Mapping(source = "measurement.id", target = "measurementId")
-    @Mapping(source = "measurement.parameter", target = "parameter")
-    @Mapping(source = "measurement.latitude", target = "latitude")
-    @Mapping(source = "measurement.longitude", target = "longitude")
-    @Mapping(source = "anomalyType", target = "anomalyType")
     AnomalyRecordResponse mapEntityToResponse(AnomalyRecord anomalyRecord);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "detectedAt", expression = "java(LocalDateTime.now())")
     @Mapping(source = "measurement", target = "measurement")
     @Mapping(source = "anomalyType", target = "anomalyType")
-    @Mapping(source = "measurement.value", target = "value")
-    AnomalyRecord mapMeasurementToAnomaly(AirQualityMeasurement measurement, AnomalyType anomalyType);
+    @Mapping(source = "deviationPercentage", target = "deviationPercentage")
+    AnomalyRecord createAnomalyRecord(AirQualityMeasurement measurement,
+                                      AnomalyType anomalyType,
+                                      Double deviationPercentage);
 }
