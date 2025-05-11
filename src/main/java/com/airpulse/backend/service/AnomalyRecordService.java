e2935209a324c6b15b1dd1fa6b27aabdc1cc63b4
Pelin <kaynarcapelin@hotmail.com>
2025-05-11 15:15:21 +0300

    refactor(service): remove unnecessary method from AnomalyRecordService

--- src/main/java/com/airpulse/backend/service/AnomalyRecordService.java
+++ src/main/java/com/airpulse/backend/service/AnomalyRecordService.java
@@ -8,21 +8,16 @@ import java.util.List;
 
 public interface AnomalyRecordService {
 
-    AnomalyRecordResponse createAnomaly(
-            Long measurementId,
-            AnomalyType anomalyType,
-            Double value,
-            Double referenceValue,
-            Double deviationPercentage);
-
     AnomalyRecordResponse getById(Long id);
 
     List<AnomalyRecordResponse> getByTimeframe(LocalDateTime start, LocalDateTime end);
 
+    // start datetime and end datetime can be null
     List<AnomalyRecordResponse> getInRegion(
             Double minLat, Double maxLat, Double minLon, Double maxLon,
             LocalDateTime start, LocalDateTime end);
 
+    // start datetime and end datetime can be null
     List<AnomalyRecordResponse> getByParameter(String parameter, LocalDateTime start, LocalDateTime end);
 
     List<AnomalyRecordResponse> getByType(AnomalyType anomalyType);
